8a8a4d48a85a33ad119dd19b81c5c5fb9dd908a7
Razvan Mirleneanu <razvan.mirleneanu@lola.tech>
2020-09-03 14:21:13 +0300

    test: add tests for existing functionalities

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,94 @@
-describe("Gilded Rose", function() {
+const ANKH = "ANKH";
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+describe("Gilded Rose", () => {
+  describe("Regular items", () => {
+    it("should decrease the quality and sell in values by 1 daily", () => {
+      items = [new Item(ANKH, 10, 10)];
+      update_quality();
+      expect(items[0].name).toEqual(ANKH);
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(9);
+    });
+    it("should decrease the quality value by 2 once the sell in date has passed", () => {
+      items = [new Item(ANKH, 0, 2)];
+      update_quality();
+      expect(items[0].name).toEqual(ANKH);
+      expect(items[0].sell_in).toEqual(-1);
+      expect(items[0].quality).toEqual(0);
+    });
+    it("should not drop the quality of an item below 0", () => {
+      items = [new Item(ANKH, 0, 2)];
+      update_quality();
+      expect(items[0].name).toEqual(ANKH);
+      expect(items[0].sell_in).toEqual(-1);
+      expect(items[0].quality).toEqual(0);
+      update_quality();
+      expect(items[0].sell_in).toEqual(-2);
+      expect(items[0].quality).toEqual(0);
+    });
+  });
+  describe("Aged Brie", () => {
+    it("should increase the quality of Aged Brie as time goes by, but never exceeding 50", () => {
+      items = [new Item(AGED_BRIE, 2, 49)];
+      update_quality();
+      expect(items[0].name).toEqual(AGED_BRIE);
+      expect(items[0].sell_in).toEqual(1);
+      expect(items[0].quality).toEqual(MAX_QUALITY_VALUE);
+      update_quality();
+      expect(items[0].sell_in).toEqual(0);
+      expect(items[0].quality).toEqual(MAX_QUALITY_VALUE);
+      update_quality();
+    });
   });
 
+  describe("Sulfuras, Hand of Ragnaros", () => {
+    it("should not update the sell in and quality properties for Sulfuras", () => {
+      items = [new Item(SULFURAS, 2, 49)];
+      update_quality();
+      expect(items[0].name).toEqual(SULFURAS);
+      expect(items[0].sell_in).toEqual(2);
+      expect(items[0].quality).toEqual(49);
+    });
+  });
+
+  describe("Backstage passes", () => {
+    it("should increase the quality of the Backstage pass by 1 when there are more than 10 days left until the concert", () => {
+      items = [new Item(BACKSTAGE_PASS, 11, 1)];
+      update_quality();
+      expect(items[0].name).toEqual(BACKSTAGE_PASS);
+      expect(items[0].sell_in).toEqual(10);
+      expect(items[0].quality).toEqual(2);
+    });
+    it("should increase the quality of the Backstage pass by 2 when there are  10 days or less left until the concert", () => {
+      items = [new Item(BACKSTAGE_PASS, 10, 1)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(3);
+    });
+    it("should increase the quality of the Backstage pass by 3 when there are  5 days or less left until the concert", () => {
+      items = [new Item(BACKSTAGE_PASS, 5, 1)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(4);
+      expect(items[0].quality).toEqual(4);
+    });
+    it("should not increase the quality of the Backstage pass above 50", () => {
+      items = [new Item(BACKSTAGE_PASS, 1, 49)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(0);
+      expect(items[0].quality).toEqual(MAX_QUALITY_VALUE);
+    });
+    it("should drop the quality of the Backstage pass to 0 after the concert", () => {
+      items = [new Item(BACKSTAGE_PASS, 0, 1)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(-1);
+      expect(items[0].quality).toEqual(0);
+    });
+    it("should not drop the quality of the Backstage pass below 0", () => {
+      items = [new Item(BACKSTAGE_PASS, 0, 1)];
+      update_quality();
+      update_quality();
+      expect(items[0].sell_in).toEqual(-2);
+      expect(items[0].quality).toEqual(0);
+    });
+  });
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,53 +1,70 @@
+const MAX_QUALITY_VALUE = 50;
+const BACKSTAGE_PASS_INCREASE_QUALITY_BY_TWO_THRESHOLD = 11;
+const BACKSTAGE_PASS_INCREASE_QUALITY_BY_THREE_THRESHOLD = 6;
+
+// Items with special rules
+const AGED_BRIE = "Aged Brie";
+const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
 function Item(name, sell_in, quality) {
+  // Name of the item
   this.name = name;
+  // Number of days available to sell the item
   this.sell_in = sell_in;
+  // The value of the item
   this.quality = quality;
 }
 
-var items = []
+var items = [];
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASS) {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+        if (items[i].name != SULFURAS) {
+          items[i].quality = items[i].quality - 1;
         }
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+      if (items[i].quality < MAX_QUALITY_VALUE) {
+        items[i].quality = items[i].quality + 1;
+        if (items[i].name == BACKSTAGE_PASS) {
+          if (
+            items[i].sell_in < BACKSTAGE_PASS_INCREASE_QUALITY_BY_TWO_THRESHOLD
+          ) {
+            if (items[i].quality < MAX_QUALITY_VALUE) {
+              items[i].quality = items[i].quality + 1;
             }
           }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+          if (
+            items[i].sell_in <
+            BACKSTAGE_PASS_INCREASE_QUALITY_BY_THREE_THRESHOLD
+          ) {
+            if (items[i].quality < MAX_QUALITY_VALUE) {
+              items[i].quality = items[i].quality + 1;
             }
           }
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+    if (items[i].name != SULFURAS) {
       items[i].sell_in = items[i].sell_in - 1;
     }
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (items[i].name != AGED_BRIE) {
+        if (items[i].name != BACKSTAGE_PASS) {
           if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+            if (items[i].name != SULFURAS) {
+              items[i].quality = items[i].quality - 1;
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = items[i].quality - items[i].quality;
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (items[i].quality < MAX_QUALITY_VALUE) {
+          items[i].quality = items[i].quality + 1;
         }
       }
     }
